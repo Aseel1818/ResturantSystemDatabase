8f7d6074ecba77035d63428882b07f42bf9a9851
Aseel Hamayel <63041611+Aseel1818@users.noreply.github.com>
2023-03-01 11:46:32 +0200

    Add files via upload

---
+++ RestaurantSystemDB/src/main/java/com/RestaurantSystemDB/RestaurantSystemDB/Repositories/OrderDetailsRepository.java
@@ -0,0 +1,13 @@
+package com.RestaurantSystemDB.RestaurantSystemDB.Repositories;
+import com.RestaurantSystemDB.RestaurantSystemDB.Models.OrderDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface OrderDetailsRepository extends JpaRepository<OrderDetails,Long> {
+
+    void deleteOrderDetailById(Long id);
+
+    Optional<OrderDetails> findOrderDetailById(Long id);
+}
---
+++ RestaurantSystemDB/src/main/java/com/RestaurantSystemDB/RestaurantSystemDB/Repositories/TablesRepository.java
@@ -0,0 +1,13 @@
+package com.RestaurantSystemDB.RestaurantSystemDB.Repositories;
+import com.RestaurantSystemDB.RestaurantSystemDB.Models.Tables;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface TablesRepository  extends JpaRepository<Tables,Long> {
+
+    void deleteTableById(Long id);
+
+    Optional<Tables> findTableById(Long id);
+}
